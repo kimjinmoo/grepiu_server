ad02dab6d699596626c49433f8d519d395335e7f
iukim21c <iukim21c@gmail.com>
2017-09-07 18:36:31 +0900

    초기

--- src/test/java/com/iuom/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/iuom/springboot/SpringbootApplicationTests.java
@@ -1,16 +1,26 @@
 package com.iuom.springboot;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SpringbootApplicationTests {
 
-//	@Test
-//	public void contextLoads() {
-//	}
+	@Autowired
+	private SqlSessionFactory sqlSession;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void connectMysql() {
+		System.out.println("mysql : " + sqlSession);
+    }
 
 }
