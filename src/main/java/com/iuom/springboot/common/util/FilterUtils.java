b51779e1566c3552fbe985cf3f5237a0449883ae
iukim21c <iukim21c@gmail.com>
2017-10-24 15:38:04 +0900

    JDK List Filter 예제 추가

---
+++ src/main/java/com/iuom/springboot/common/util/FilterUtils.java
@@ -0,0 +1,34 @@
+package com.iuom.springboot.common.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ *
+ * 필터 Utilt이다.
+ *
+ * @author JM
+ * @Since 2017.10.24/v1.0
+ */
+public class FilterUtils {
+
+    /**
+     *
+     * 필터를 통해 값을 가져온다.
+     *
+     * @param list
+     * @param p
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> listFilter(List<T> list, Predicate<T> p) {
+        List<T> result = new ArrayList<>();
+        for(T obj : list) {
+            if(p.test(obj)) {
+                result.add(obj);
+            }
+        }
+        return result;
+    }
+}
