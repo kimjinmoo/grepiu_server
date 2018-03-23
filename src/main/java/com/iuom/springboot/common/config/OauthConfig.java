537a2f989e444fc2f765697563430583d508440a
DESKTOP-HL2MB8A\developer <iukim21c@gmail.com>
2018-03-23 17:50:46 +0900

    1. TDD 보안

--- src/main/java/com/iuom/springboot/common/config/OauthConfig.java
+++ src/main/java/com/iuom/springboot/common/config/OauthConfig.java
@@ -1,32 +1,23 @@
 package com.iuom.springboot.common.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class OauthConfig extends WebSecurityConfigurerAdapter  {
 
-
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//    auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
-    auth.inMemoryAuthentication()
-        .withUser("user")
-        .password("password")
-        .roles("USER")
-        .and()
-    .withUser("admin")
-        .password("password")
-        .roles("ADMIN","USER");
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser(User.withDefaultPasswordEncoder().username("iukim21c").password("iukim21c").roles("USER"));
   }
 
   @Override
@@ -39,27 +30,21 @@ public class OauthConfig extends WebSecurityConfigurerAdapter  {
         .antMatchers("/swagger-ui.html").hasRole("USER")
         .anyRequest().authenticated()
         .and()
-        .formLogin().and().httpBasic();
+        .formLogin().and().httpBasic()
+        .and()
+        .logout().logoutUrl("/logout");
   }
 
-
-  private LoginUrlAuthenticationEntryPoint oauth2AuthenticationEntryPoint() {
-    return new LoginUrlAuthenticationEntryPoint("/login");
-  }
-
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-//    authProvider.setUserDetailsService(userService);
-    authProvider.setPasswordEncoder(passwordEncoder());
-    return authProvider;
-  }
+//  @Bean
+//  public DaoAuthenticationProvider authenticationProvider() {
+//    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//    authProvider.setPasswordEncoder(passwordEncoder());
+//    return authProvider;
+//  }
 
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    // Create an encoder with strength 31
-    // values from 4 .. 31 are valid; the higher the value, the more work has to be done to calculate the hash
     return new BCryptPasswordEncoder(12);
   }
 }
--- src/main/java/com/iuom/springboot/common/config/auth/service/UserService.java
+++ src/main/java/com/iuom/springboot/common/config/auth/service/UserService.java
@@ -1,8 +1,5 @@
 package com.iuom.springboot.common.config.auth.service;
 
-import com.iuom.springboot.common.config.auth.dao.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,15 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService implements UserDetailsService {
 
-  @Autowired
-  UserRepository userRepository;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userRepository.findByUsername(username);
-    if(user == null) {
-        throw new UsernameNotFoundException(username);
-    }
-    return  user;
+//    User user = userRepository.findByUsername(username);
+//    if(user == null) {
+//        throw new UsernameNotFoundException(username);
+//    }
+    return  null;
   }
 }
