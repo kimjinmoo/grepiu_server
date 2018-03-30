2ac8b9923e46f99c062fcc2f6eea34650c36130c
DESKTOP-HL2MB8A\developer <iukim21c@gmail.com>
2018-03-30 16:31:28 +0900

    1. test

--- src/main/java/com/iuom/springboot/common/config/ResourceServerConfig.java
+++ src/main/java/com/iuom/springboot/common/config/ResourceServerConfig.java
@@ -1,36 +1,36 @@
-package com.iuom.springboot.common.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
-
-@Configuration
-@EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter  {
-
-  private static final String RESOURCE_ID = "resource_id";
-
-  @Override
-  public void configure(ResourceServerSecurityConfigurer resources) {
-    resources.resourceId(RESOURCE_ID).stateless(false);
-  }
-
-  @Override
-  public void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests()
-        // 일반적인 Open 정책
-        .antMatchers("/resources/**/*", "/webjars/**", "/ws/**/*", "/app/**", "/topic/messages")
-        .permitAll()
-        .antMatchers("/", "/home").permitAll()
-        .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources",
-            "/configuration/security", "/swagger-resources/configuration/ui",
-            "/swagger-resources/configuration/security", "/swagger-ui.html", "/null/**").hasAuthority("USER")
-        .anyRequest().authenticated()
-        .and()
-        .formLogin().usernameParameter("email").permitAll().and()
-        .exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
-  }
-}
+//package com.iuom.springboot.common.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+//
+//@Configuration
+//@EnableResourceServer
+//public class ResourceServerConfig extends ResourceServerConfigurerAdapter  {
+//
+//  private static final String RESOURCE_ID = "resource_id";
+//
+//  @Override
+//  public void configure(ResourceServerSecurityConfigurer resources) {
+//    resources.resourceId(RESOURCE_ID).stateless(false);
+//  }
+//
+//  @Override
+//  public void configure(HttpSecurity http) throws Exception {
+//    http.csrf().disable().authorizeRequests()
+//        // 일반적인 Open 정책
+//        .antMatchers("/resources/**/*", "/webjars/**", "/ws/**/*", "/app/**", "/topic/messages")
+//        .permitAll()
+//        .antMatchers("/", "/home").permitAll()
+//        .antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources",
+//            "/configuration/security", "/swagger-resources/configuration/ui",
+//            "/swagger-resources/configuration/security", "/swagger-ui.html", "/null/**").hasAuthority("USER")
+//        .anyRequest().authenticated()
+//        .and()
+//        .formLogin().usernameParameter("email").permitAll().and()
+//        .exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
+//  }
+//}
