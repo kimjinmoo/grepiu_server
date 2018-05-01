2808c4c26b5033ef98c78d0d8c5e0522ac9a6259
DESKTOP-HL2MB8A\developer <iukim21c@gmail.com>
2018-05-01 16:34:39 +0900

    1. 롯데시네마 Json to Mongo DB 추가

---
+++ src/main/java/com/iuom/springboot/common/util/MapUtils.java
@@ -0,0 +1,46 @@
+package com.iuom.springboot.common.util;
+
+import com.iuom.springboot.common.util.domain.MapGoogleVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ *
+ * 지도 Helper
+ *
+ */
+@Slf4j
+public class MapUtils {
+  private String googleGeoCodingApi = "https://maps.googleapis.com/maps/api/geocode/json";
+
+  /**
+   *
+   * 주소를 받아서 좌표 값으로 변환한다.
+   *
+   * @param address
+   * @return
+   */
+  public MapGoogleVO searchLocalePointWithGoogle(String address) {
+    RestTemplate restTemplate = new RestTemplate();
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+    UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(googleGeoCodingApi)
+        .queryParam("address", address)
+        .queryParam("key", "AIzaSyCdlJJ5RuA6jrq-VzexA-8ryqOJVqTJJ-Y");
+    HttpEntity<?> entity = new HttpEntity<>(headers);
+
+    HttpEntity<MapGoogleVO> response = restTemplate.exchange(
+        builder.build(false).toString(),
+        HttpMethod.GET,
+        entity,
+        MapGoogleVO.class);
+    return response.getBody();
+  }
+
+}
