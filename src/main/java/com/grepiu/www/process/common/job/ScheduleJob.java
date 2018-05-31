9f058b2b95cee72b4aa4eb3c8f6c677317851091
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-05-31 10:46:36 +0900

    1. 스프링 Boot 버전 2.0.2로 변경, 실행 script 추가 2. 배치 시간 변경

--- src/main/java/com/grepiu/www/process/common/job/ScheduleJob.java
+++ src/main/java/com/grepiu/www/process/common/job/ScheduleJob.java
@@ -53,10 +53,11 @@ public class ScheduleJob {
 
   /**
    * 크롤링 스케쥴러
-   *
-   * 6시간에 한번씩 구현
+   * <second> <minute> <hour> <day-of-month> <month> <day-of-week> <year> <command>
+   * 매일 07시,12,17시
    */
-  @Scheduled(fixedDelay = 1000*60*60*6)
+//  @Scheduled(fixedDelay = 1000*60*60*3)
+  @Scheduled(cron="00 00 07,12,17 * * ?")
   public void crawler() throws Exception {
     log.info(" start crawler======================= {}", DateUtil.now("yyyy/MM/dd hh:mm:ss"));
     //step1. Collect Data
