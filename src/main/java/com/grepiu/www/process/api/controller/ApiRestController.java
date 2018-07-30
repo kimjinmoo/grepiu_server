dab81cd3203d1e78a142647df1e8686a9650d54a
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-07-30 16:18:51 +0900

    1. YAML profiles 설정

--- src/main/java/com/grepiu/www/process/api/controller/ApiRestController.java
+++ src/main/java/com/grepiu/www/process/api/controller/ApiRestController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * API 컨트롤러 이다.
  *
  */
+@RequestMapping("/api")
 @RestController
 public class ApiRestController {
 
