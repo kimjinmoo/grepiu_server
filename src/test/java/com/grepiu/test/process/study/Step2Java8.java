3cc9e9522a174bc2b7d257280162c79b1cf37e50
iukim21c <iukim21c@gmail.com>
2018-10-07 00:37:34 +0900

    1. 스터디 자료

---
+++ src/test/java/com/grepiu/test/process/study/Step2Java8.java
@@ -0,0 +1,54 @@
+package com.grepiu.test.process.study;
+
+/**
+ *
+ * functional interface
+ *
+ */
+public class Step2Java8 {
+    public static void main(String...args) {
+        StringToNumber<String, Integer> toString = (String s) -> {
+            return Integer.valueOf(s);
+        };
+        // 출력
+        System.out.println("test : " + toString.run("2"));
+
+        // 외부 변수
+        int sampleNum = 0;
+        Print<Integer> print = (Integer i) -> {
+            System.out.println("free variable : "+sampleNum);
+            System.out.println(i);
+        };
+
+        print.out(1);
+
+        Print<String> carPrint = Car::getCarModel;
+        carPrint.out("마티즈");
+
+        Animal dog = new Animal();
+        Print<String> animal = dog::bark;
+        animal.out("멍멍");
+    }
+}
+
+@FunctionalInterface
+interface StringToNumber<T, R> {
+    R run(T t);
+}
+
+@FunctionalInterface
+interface Print<T> {
+    void out(T t);
+}
+
+class Car {
+    public static void  getCarModel(String model){
+        System.out.println(model);
+    }
+}
+
+class Animal {
+    public void bark(String sound) {
+        System.out.println(sound);
+    }
+}
